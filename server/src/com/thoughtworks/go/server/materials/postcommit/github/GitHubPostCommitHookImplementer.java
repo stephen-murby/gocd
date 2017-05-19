dbc5bc8dba96826229b7e7b293fd851f0a4396c6
Stephen Murby <stephen.murby@tryteksystems.co.uk>
2017-05-19 13:06:15 +0100

    Refactored the prune method to exclude non push event messages and clean up using streams.

--- server/src/com/thoughtworks/go/server/materials/postcommit/github/GitHubPostCommitHookImplementer.java
+++ server/src/com/thoughtworks/go/server/materials/postcommit/github/GitHubPostCommitHookImplementer.java
@@ -16,6 +16,7 @@
 
 package com.thoughtworks.go.server.materials.postcommit.github;
 
+import com.google.common.collect.Sets;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.thoughtworks.go.config.materials.git.GitMaterial;
@@ -31,16 +32,14 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class GitHubPostCommitHookImplementer implements PostCommitHookImplementer {
 
-//    final String REPO_URL_PARAM_KEY = "url";
-//    final String REPO_SSH_URL_PARAM_KEY = "ssh_url";
-//    final String REPO_CLONE_URL_PARAM_KEY = "clone_url";
-//    final String REPO_SVN_URL_PARAM_KEY = "svn_url";
-
-    final String REPO_GIT_URL_PARAM_KEY = "url";
-    private String REPO_OBJECT_KEY = "repository";
+    private final String REPO_GIT_URL_PARAM_KEY = "url";
+    private final String GITHUB_REPOSITORY = "repository";
+    private final String GITHUB_PUSH_EVENT_IDENTIFIER = "commits";
 
     private final UrlMatchers validators = new UrlMatchers();
 
@@ -48,28 +47,40 @@ public class GitHubPostCommitHookImplementer implements PostCommitHookImplemente
     public Set<Material> prune(Set<Material> materials, Map params) {
 
         HashSet<Material> prunedCollection = new HashSet<>();
-        JsonObject decodedParams;
+        JsonObject webhookPayload;
         try {
-            decodedParams = parsePayload(decodePayload(params));
+            webhookPayload = parsePayload(decodePayload(params));
         } catch (UnsupportedEncodingException e) {
             return prunedCollection;
         }
 
-        if( decodedParams.getAsJsonObject("repository") != null
-                && decodedParams.getAsJsonObject(REPO_OBJECT_KEY).get(REPO_GIT_URL_PARAM_KEY).getAsString() != null
-                && !decodedParams.getAsJsonObject(REPO_OBJECT_KEY).get(REPO_GIT_URL_PARAM_KEY).getAsString().isEmpty()
-                ) {
-            String paramRepoUrl = decodedParams.getAsJsonObject(REPO_OBJECT_KEY).get("url").getAsString();
-            String repoFullName = decodedParams.getAsJsonObject(REPO_OBJECT_KEY).get("full_name").getAsString();
-            for (Material material : materials) {
-                if (material instanceof GitMaterial && isUrlEqual(paramRepoUrl, (GitMaterial) material, repoFullName)) {
-                    prunedCollection.add(material);
-                }
-            }
-            return prunedCollection;
-        }else {
-            return prunedCollection;
+        if (isGitHubPushEvent(webhookPayload)) {
+            JsonObject repository = webhookPayload.getAsJsonObject(GITHUB_REPOSITORY);
+            String repositoryUrl = repository.get("url").getAsString();
+            String repositoryFullName = repository.get("full_name").getAsString();
+
+            return materials.stream()
+                    .filter(material -> this.matchesGithubRepository(material, repositoryUrl, repositoryFullName))
+                    .collect(Collectors.toSet());
         }
+        return Sets.newHashSet();
+    }
+
+    private boolean matchesGithubRepository(Material material, String repoUrl, String repoFullName) {
+        if ( material instanceof GitMaterial
+                && isUrlEqual(repoUrl, (GitMaterial) material, repoFullName)) {
+            return true;
+        } else return false;
+    }
+
+    private boolean hasRepositoryInPayload(JsonObject decodedParams) {
+        return decodedParams.getAsJsonObject("repository") != null
+                && decodedParams.getAsJsonObject(GITHUB_REPOSITORY).get(REPO_GIT_URL_PARAM_KEY).getAsString() != null
+                && !decodedParams.getAsJsonObject(GITHUB_REPOSITORY).get(REPO_GIT_URL_PARAM_KEY).getAsString().isEmpty();
+    }
+
+    private boolean isGitHubPushEvent(JsonObject webhookPayload) {
+        return webhookPayload.has(GITHUB_PUSH_EVENT_IDENTIFIER) && hasRepositoryInPayload(webhookPayload);
     }
 
     private JsonObject parsePayload(String payload) {
